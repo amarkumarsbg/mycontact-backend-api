f29889cb7b302ae235a71eb327648706c3d77029
amarkumarsbg <amarkumarsbg13@gmail.com>
2023-09-20 23:30:14 +0530

    CRUD part is working fine

---
+++ controllers/contactController.js
@@ -0,0 +1,77 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+//@desc Get all contacts
+//@route Get  /api/contacts
+//@access public
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
+
+//@desc Create New contacts
+//@route POST  /api/contacts
+//@access public
+const createContact = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fileds are mandatory");
+  }
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(200).json(contact);
+});
+
+//@desc Get contacts
+//@route GET  /api/contacts/:id
+//@access public
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  res.status(200).json(contact);
+});
+
+//@desc Update contacts
+//@route GET  /api/contacts/:id
+//@access public
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
+});
+//@desc Delete contacts
+//@route DELETE  /api/contacts/:id
+//@access public
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  await Contact.deleteOne();
+  res.status(200).json(contact);
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
